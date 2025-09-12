da2bb86409b4404bd1bee7f46a0e124935b77247
chunim98 <jjingeo1230@naver.com>
2025-09-12 18:01:59 +0900

    1012 유기농 배추: BFS

--- SwiftAlgorithm/Baekjoon/1012 유기농 배추.swift
+++ SwiftAlgorithm/Baekjoon/1012 유기농 배추.swift
@@ -61,3 +61,60 @@ func problem1012() {
     // 케이스 개수만큼 실행
     for _ in 0..<t { dfsSolution() }
 }
+
+// MARK: BFS
+
+//func problem1012() {
+//    let t = Int(readLine()!)!
+//    
+//    for _ in 0..<t {
+//        let input = readLine()!.split(separator: " ").map { Int($0)! }
+//        let (m, n, k) = (input[0], input[1], input[2]) // 가로 세로 배추갯수
+//        
+//        var graph = [[Int]](repeating: .init(repeating: 0, count: m), count: n)
+//        
+//        for _ in 0..<k {
+//            let input = readLine()!.split(separator: " ").map { Int($0)! }
+//            let (x, y) = (input[0], input[1])
+//            graph[y][x] = 1
+//        }
+//        
+//        func bfs(x: Int, y: Int) {
+//            var queue = [(x, y)]
+//            
+//            let dx = [1, -1, 0, 0]
+//            let dy = [0, 0, 1, -1]
+//            
+//            while !queue.isEmpty {
+//                let (x, y) = queue.removeFirst()
+//                
+//                for i in 0..<4 {
+//                    let nx = x + dx[i]
+//                    let ny = y + dy[i]
+//                    
+//                    guard nx >= 0,
+//                          nx < m,
+//                          ny >= 0,
+//                          ny < n,
+//                          graph[ny][nx] == 1
+//                    else { continue }
+//                    
+//                    queue.append((nx, ny))
+//                    graph[ny][nx] = 0
+//                }
+//            }
+//        }
+//        
+//        var result = 0
+//        
+//        for y in 0..<n {
+//            for x in 0..<m {
+//                guard graph[y][x] == 1 else { continue }
+//                result += 1
+//                bfs(x: x, y: y)
+//            }
+//        }
+//        
+//        print(result)
+//    }
+//}
